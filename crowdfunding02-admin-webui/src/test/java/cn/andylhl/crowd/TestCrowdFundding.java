21e748ede1de63f7a925b6ba68e219d0976fd354
lhllovezj <lhllovezj@gmail.com>
2020-12-20 19:31:37 +0800

    集成日志-使用slf4j+logback

--- crowdfunding02-admin-webui/src/test/java/cn/andylhl/crowd/TestCrowdFundding.java
+++ crowdfunding02-admin-webui/src/test/java/cn/andylhl/crowd/TestCrowdFundding.java
@@ -7,14 +7,13 @@ import cn.andylhl.crowd.utils.DateUtil;
 import cn.andylhl.crowd.utils.UUIDUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Date;
-import java.util.UUID;
 
 /***
  * @Title: TestCrowdFundding
@@ -25,7 +24,7 @@ import java.util.UUID;
 
 //Spring整和junit用于单元测试
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:config/spring-persist-mybatis.xml")
+@ContextConfiguration(locations = "classpath:spring-persist-mybatis.xml")
 public class TestCrowdFundding {
 
     //可以在类中注入ioc容器的对象
@@ -34,9 +33,31 @@ public class TestCrowdFundding {
 
     @Test
     public void testConnect(){
-        Admin admin = new Admin(UUIDUtil.getUUID(), "zs", "123", "张三", "zs@qq.com", DateUtil.format(new Date(), Const.DATE_Format_ALL));
+        Admin admin = new Admin(UUIDUtil.getUUID(), "ww", "123", "王五", "ww@qq.com", DateUtil.format(new Date(), Const.DATE_Format_ALL));
         int count = adminMapper.insert(admin);
         System.out.println(count == 1 ? "插入成功" : "插入失败");
     }
 
+    @Test
+    public void testLogger(){
+        //获取打印日志信息的对象，这里传入的Class就是当前打印日志的类
+        Logger logger =  LoggerFactory.getLogger(TestCrowdFundding.class);
+
+        logger.debug("debug level");
+        logger.debug("debug level");
+        logger.debug("debug level");
+
+        logger.info("info level");
+        logger.info("info level");
+        logger.info("info level");
+
+        logger.warn("warn level");
+        logger.warn("warn level");
+        logger.warn("warn level");
+
+        logger.error("error level");
+        logger.error("error level");
+        logger.error("error level");
+
+    }
 }
